1f1b6ebcd131e4a01026be566cc1ec6f924e1974
BojungChoi <fake5378@naver.com>
2025-04-24 17:21:00 +0900

    day05_Commit

---
+++ day05/Day05Study/SyntaxWinApp02/FrmMain.cs
@@ -0,0 +1,126 @@
+﻿namespace SyntaxWinApp02
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        int add(int x, int y)
+        {
+            return x + y; // 한줄짜리
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            int result = add(10, 5);
+
+            //        대리자         = 람다식
+            Func<int, int, int> add2 = (x, y) => x + y;
+            TxtLog.Text += result + "\r\n";
+            TxtLog.Text += add2(10, 6) + "\r\n";
+
+
+            // SayHello 함수 생성대신
+            Action<string> sayHello = name => MessageBox.Show($"안녕,{name}", "인사",
+                                                MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            sayHello("뽀룡");
+
+            // LINQ 사용 이전
+            List<int> resList = new List<int>();
+            List<int> numbers = [4, 10, 1, 5, 9, 8, 6, 2, 3, 7];
+
+            // 짝수만 추출해서 오름차순 정렬 하는 로직
+            foreach (int n in numbers)
+            {
+                if (n % 2 == 0)
+                {
+                    resList.Add(n);
+                }
+            }
+            TxtLog.Text += "전통 짝수리스트 > " + string.Join(", ", resList) + "\r\n";
+
+
+            resList.Sort();
+
+            //TxtLog.Text += "정렬리스트" + resList + "\r\n";
+            TxtLog.Text += "전통 정렬리스트 > " + string.Join(", ", resList) + "\r\n";
+
+            // 기본 LINQ 방식 > 줄로 위의 전통방식을 처리
+            numbers = [14, 20, 11, 15, 18, 19, 16, 13, 12, 17];
+            var resList2 = from n in numbers
+                           where n % 2 == 0
+                           orderby n
+                           select n;
+            TxtLog.Text += "링큐1 정렬리스트 > " + string.Join(",", resList2) + "\r\n";
+
+            // LINQ Method Chaining
+            numbers = [24, 30, 21, 25, 28, 29, 26, 23, 22, 27];
+            var resList3 = numbers.Where(n => n % 2 == 0).OrderBy(n => n);
+            TxtLog.Text += "링큐2 정렬리스트 > " + string.Join(",", resList3) + "\r\n";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // UI 초기화
+            TxtTest.PlaceholderText = "테스트입니다.";
+            TxtTest.Size = new Size(250, 23);
+            TxtTest.KeyPress += TxtTest_KeyPress;
+            TxtTest.Font = new Font("나눔고딕", 10, FontStyle.Italic);
+        }
+        private void TxtTest_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)  // == Keys.Enter와 동일
+            {
+                MessageBox.Show("엔터를 쳤습니다.", "키프레스", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void BtnGeneric_Click(object sender, EventArgs e)
+        {
+            // 기본
+            Print<int>(100);
+            Print<float>(3.141592f);
+            Print<string>("안녕,뽀룡!");
+            Print<Boolean>(false);
+
+            // 생략가능 - 편의성을 위해서
+            Print(200);
+            Print(5.141592f);
+            Print("잘가,뽀룡!");
+            Print(true);
+
+            // 제네릭 클래스 사용
+            Box<int> intBox = new Box<int>();
+            intBox.Value = 300;
+            intBox.Show();
+
+            Box<string> stringBox = new Box<string>();
+            stringBox.Value = "뽀룡스";
+            stringBox.Show();
+
+        }
+
+        public void Print<T>(T data) { Console.WriteLine(data); }
+    }
+
+    // 제네릭 클래스
+    //public class Box<T> where T : struct // <- 이거넣으면 string 안됨. (값형식만받기)
+    //public class Box<T> where T : class // <- 이거넣으면 int 는 안됨. (클래스만 허용)
+    public class Box<T> 
+    {
+
+        public T Value { get; set; } // 속성
+        // private T value; // 멤버변수
+
+        public void Show()
+        {
+            MessageBox.Show($"Box의 값 : {Value}","BOX값" , MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+    }
+}
---
+++ day05/Day05Study/SyntaxWinApp03/FrmMain.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SyntaxWinApp03
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            BtnStart = new Button();
+            TxtLog = new TextBox();
+            label1 = new Label();
+            LblCurrState = new ToolStripStatusLabel();
+            PrgProcess = new ToolStripProgressBar();
+            statusStrip1 = new StatusStrip();
+            toolStripDropDownButton1 = new ToolStripDropDownButton();
+            기모찌ToolStripMenuItem = new ToolStripMenuItem();
+            넘모좋아잉ToolStripMenuItem = new ToolStripMenuItem();
+            아주좋아잉ToolStripMenuItem = new ToolStripMenuItem();
+            좋와잉ToolStripMenuItem = new ToolStripMenuItem();
+            좋아ToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            BtnStart.Location = new Point(472, 256);
+            BtnStart.Name = "BtnStart";
+            BtnStart.Size = new Size(100, 30);
+            BtnStart.TabIndex = 0;
+            BtnStart.Text = "시작";
+            BtnStart.UseVisualStyleBackColor = true;
+            BtnStart.Click += BtnStart_Click;
+            // 
+            // TxtLog
+            // 
+            TxtLog.BorderStyle = BorderStyle.FixedSingle;
+            TxtLog.Location = new Point(12, 27);
+            TxtLog.Multiline = true;
+            TxtLog.Name = "TxtLog";
+            TxtLog.ScrollBars = ScrollBars.Vertical;
+            TxtLog.Size = new Size(560, 223);
+            TxtLog.TabIndex = 1;
+            TxtLog.TextChanged += TxtLog_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 2;
+            label1.Text = "로그";
+            // 
+            // LblCurrState
+            // 
+            LblCurrState.Name = "LblCurrState";
+            LblCurrState.Size = new Size(90, 17);
+            LblCurrState.Text = "현재상태 : 중지";
+            // 
+            // PrgProcess
+            // 
+            PrgProcess.Name = "PrgProcess";
+            PrgProcess.Size = new Size(380, 16);
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { LblCurrState, PrgProcess, toolStripDropDownButton1 });
+            statusStrip1.Location = new Point(0, 289);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(584, 22);
+            statusStrip1.SizingGrip = false;
+            statusStrip1.TabIndex = 3;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            toolStripDropDownButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripDropDownButton1.DropDownItems.AddRange(new ToolStripItem[] { 기모찌ToolStripMenuItem, 넘모좋아잉ToolStripMenuItem, 아주좋아잉ToolStripMenuItem, 좋와잉ToolStripMenuItem, 좋아ToolStripMenuItem });
+            toolStripDropDownButton1.Image = (Image)resources.GetObject("toolStripDropDownButton1.Image");
+            toolStripDropDownButton1.ImageTransparentColor = Color.Magenta;
+            toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            toolStripDropDownButton1.Size = new Size(29, 20);
+            toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            // 
+            // 기모찌ToolStripMenuItem
+            // 
+            기모찌ToolStripMenuItem.Name = "기모찌ToolStripMenuItem";
+            기모찌ToolStripMenuItem.Size = new Size(138, 22);
+            기모찌ToolStripMenuItem.Text = "기모찌";
+            // 
+            // 넘모좋아잉ToolStripMenuItem
+            // 
+            넘모좋아잉ToolStripMenuItem.Name = "넘모좋아잉ToolStripMenuItem";
+            넘모좋아잉ToolStripMenuItem.Size = new Size(138, 22);
+            넘모좋아잉ToolStripMenuItem.Text = "넘모 좋아잉";
+            // 
+            // 아주좋아잉ToolStripMenuItem
+            // 
+            아주좋아잉ToolStripMenuItem.Name = "아주좋아잉ToolStripMenuItem";
+            아주좋아잉ToolStripMenuItem.Size = new Size(138, 22);
+            아주좋아잉ToolStripMenuItem.Text = "아주좋아잉";
+            // 
+            // 좋와잉ToolStripMenuItem
+            // 
+            좋와잉ToolStripMenuItem.Name = "좋와잉ToolStripMenuItem";
+            좋와잉ToolStripMenuItem.Size = new Size(138, 22);
+            좋와잉ToolStripMenuItem.Text = "좋와잉";
+            // 
+            // 좋아ToolStripMenuItem
+            // 
+            좋아ToolStripMenuItem.Name = "좋아ToolStripMenuItem";
+            좋아ToolStripMenuItem.Size = new Size(138, 22);
+            좋아ToolStripMenuItem.Text = "좋아";
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 311);
+            Controls.Add(statusStrip1);
+            Controls.Add(label1);
+            Controls.Add(TxtLog);
+            Controls.Add(BtnStart);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "FrmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "스레드 연습";
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BtnStart;
+        private TextBox TxtLog;
+        private Label label1;
+        private ToolStripStatusLabel LblCurrState;
+        private ToolStripProgressBar PrgProcess;
+        private StatusStrip statusStrip1;
+        private ToolStripDropDownButton toolStripDropDownButton1;
+        private ToolStripMenuItem 기모찌ToolStripMenuItem;
+        private ToolStripMenuItem 넘모좋아잉ToolStripMenuItem;
+        private ToolStripMenuItem 아주좋아잉ToolStripMenuItem;
+        private ToolStripMenuItem 좋와잉ToolStripMenuItem;
+        private ToolStripMenuItem 좋아ToolStripMenuItem;
+    }
+}
